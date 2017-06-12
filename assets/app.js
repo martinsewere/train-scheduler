09d91127837d2ceeaef243c7bc75143b03a73ab1
Martins Ewere <martinsewere@gmail.com>
2017-06-11 21:23:16 -0700

    edit assets/app.js and index.html

--- assets/app.js
+++ assets/app.js
@@ -13,41 +13,70 @@
 var database = firebase.database();
 
 
-var trainName="";
-var Destination="";
-var Frequency="";
+var name="";
+var destination="";
+var frequency="";
 var nextArrival="";
 var minutesAway="";
+var inputTrain="";
 
 
 
-//On submit click, grab the values from the fields.
+//On click button, grab the values from the input fields.
 
 $("#add-train").on("click", function(event) {
+  console.log(event);
 
     event.preventDefault();
 
-trainName = $("#Train-Name").val().trim();
-Destination = $("#Destination").val().trim();
-Frequency = $("#Frequency").val().trim();
-nextArrival = $("#Next-Arrival").val().trim();
-minutesAway = $("#Minutes-Away").val().trim();
+// Takes information from the html form and stores them into variables
+name = $("#name").val().trim();
+destination = $("#destination").val().trim();
+frequency = $("#frequency").val().trim();
+nextArrival = $("#nextArrival").val().trim();
+minutesAway = $("#minutesAway").val().trim();
+
+convertedTime = moment(nextArrival, "hh:mm").subtract(1, "years");
+console.log("Converted Time " + convertedTime);
+timeDiff = moment().diff(moment(convertedTime), "minutes");
+console.log("Difference in Time " + timeDiff);
+tRemainder = timeDiff % frequency;
+console.log("Modulus " + tRemainder);
+minRemain = frequency - tRemainder;
+nextArrival = moment().add(minRemain, "minutes").format("hh:mm");
+console.log(nextArrival);
 
 
-
-var newObject = {
-    trainName: Train-Name,
-    Destination: Destination,
-    Frequency: Frequency,
-    nextArrival: Next-Arrival,
-    minutesAway: Minutes-Away,
+// Stores data in firebase database
+var inputTrain= {
+    name: name,
+    destination: destination,
+    frequency: frequency,
+    nextArrival: nextArrival,
+    minutesAway: minutesAway,
 }
 
-database.ref().push(
-    newObject
-);
+database.ref().push(inputTrain);
+
+$("#name").val("");
+$("#destination").val("");
+$("#frequency").val("");
+$("#nextArrival").val("");
+
 });
-console.log(newobject);
+console.log(inputTrain);
+
+database.ref().on("child_added", function(childSnapshot) {
+
+  name = childSnapshot.val().name;
+  destination = childSnapshot.val().destination;
+  frequency = childSnapshot.val().frequency;
+  nextArrival = childSnapshot.val().nextArrival;
+  minutesAway = childSnapshot.val().minutesAway;
+
+  $(".table").append("<tr><td>" + name + "</td><td>" + destination + "</td><td>" + frequency + "</td><td>" + nextArrival + "</td><td>" + minutesAway + "</td>")
+
+})
 
 
 
